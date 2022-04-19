77809f03d503b5af4f1edc33d5e376eb3e260482
muratpala007 <muratpala007@gmail.com>
2022-04-19 16:05:31 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45DersMetotlardaOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EGER AYNI ISIMDE ANCAK FARKLI PARAMETRE ALAN BIRDEN FAZLA METOT OLUSTURURSAK OVERLOADING YAPMIS OLURUZ.
+            //FONKSIYONU CAGIRIRKEN KULANDIGIMIZ PARAMETRE SAYISINA GORE ILGILI METOTU KENDI BULUR...
+            //SADECE PARAMETRE SAYISI DEGIL, PARAMETRE TURUNU FARKLILASTIRARAK DA OVERLOADING YAPABILIRIZ (INT, DOUBLE VS.)
+            Console.WriteLine(isimSoyle("Murat")); 
+            Console.WriteLine(isimSoyle("Murat","Pala"));
+
+            Console.ReadLine();
+        }
+
+        static string isimSoyle(string isim)
+        {
+            return "Tek parametreli metot: " + isim;
+        }
+
+        static string isimSoyle(string isim, string soyIsim)
+        {
+            return "İki parametreli metot: " + isim + " " + soyIsim;
+        }
+    }
+}
